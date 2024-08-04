b86607ff7cfdf68c6c8cbf1070416b2be1c07399
Anıl <kardasland@hotmail.com>
2024-08-05 01:07:15 +0300

    Added delay system.

--- src/main/java/com/kardasland/aetherpotions/potion/CCommand.java
+++ src/main/java/com/kardasland/aetherpotions/potion/CCommand.java
@@ -3,6 +3,7 @@ package com.kardasland.aetherpotions.potion;
 import com.kardasland.aetherpotions.AetherPotions;
 import lombok.Data;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
 
@@ -12,14 +13,16 @@ public class CCommand {
     List<String> command;
     int chance;
     int weight;
+    int delay;
     Executor executor;
 
-    public CCommand(int order, List<String> command, int chance, int weight, Executor executor) {
+    public CCommand(int order, List<String> command, int chance, int weight, Executor executor, int delay) {
         this.order = order;
         this.command = command;
         this.chance = chance;
         this.weight = weight;
         this.executor = executor;
+        this.delay = delay;
     }
     public CCommand() {
     }
@@ -35,13 +38,40 @@ public class CCommand {
     public boolean isFixed() {
         return !isChance() && !isWeight();
     }
+    public boolean isDelayed() {
+        return delay > 0;
+    }
 
+
+    // alright fuck it i cant find a way to effectively chain delays.
+    // RECURSIVE TIME MOTHERFUCKER
     public void execute(Player target){
         for (String s : command) {
-            s = s.replace("%player%", target.getName()).replace("%target%", target.getName());
-            AetherPotions.instance.getServer().dispatchCommand(executor.equals(Executor.CONSOLE) ? AetherPotions.instance.getServer().getConsoleSender() : target, s);
+            final String command = s.replace("%player%", target.getName()).replace("%target%", target.getName());
+            if (delay > 0) {
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        AetherPotions.instance.getServer().dispatchCommand(executor.equals(Executor.CONSOLE) ? AetherPotions.instance.getServer().getConsoleSender() : target, command);
+                    }
+                }.runTaskLater(AetherPotions.instance, delay * 20L);
+                return;
+            }
+            AetherPotions.instance.getServer().dispatchCommand(executor.equals(Executor.CONSOLE) ? AetherPotions.instance.getServer().getConsoleSender() : target, command);
         }
     }
+
+
+    public void execute(Player target, List<CCommand> e){
+        if (e.isEmpty()){
+            return;
+        }
+        CCommand command1 = e.get(0);
+        command1.execute(target);
+        e.remove(command1);
+        execute(target, e);
+    }
+
 
     public enum Executor {
         CONSOLE,
@@ -56,6 +86,7 @@ public class CCommand {
                 ", chance=" + chance +
                 ", weight=" + weight +
                 ", executor=" + executor +
+                ", delay=" + delay +
                 '}';
     }
 }
